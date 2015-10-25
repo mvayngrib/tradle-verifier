575576513ab7673db0851e1b52a9cc2a68e6b69e
Mark Vayngrib <mark.vayngrib@lablz.com>
2015-10-25 16:02:12 +0000

    change sigcheck to accept SIGNEE field if set

--- plugins/sigcheck.js
+++ plugins/sigcheck.js
@@ -4,6 +4,7 @@ var Builder = require('chained-obj').Builder
 var Identity = require('midentity').Identity
 var constants = require('tradle-constants')
 var parallel = require('run-parallel')
+var SIGNEE = constants.SIGNEE
 var SIG = constants.SIG
 var TYPE = constants.TYPE
 var PREV_HASH = constants.PREV_HASH
@@ -15,42 +16,64 @@ module.exports = {
     // unsigned objects are fine, they don't claim to be created by anyone
     if (typeof sig === 'undefined') return next(verifier, chainedObj, cb)
 
-    var identity = chainedObj.from && chainedObj.from.identity
-    if (!identity) return cb(new Error('unknown sender'))
+    getSignee(verify)
 
-    var isIdentity = data[TYPE] === Identity.TYPE
-    var purpose
-    if (isIdentity && data[PREV_HASH]) {
-      purpose = 'update'
-    } else {
-      purpose = 'sign'
+    function getSignee (cb) {
+      if (data[SIGNEE]) {
+        // format is one of:
+        // rootHash:curHash
+        // curHash
+        var curHash = data[SIGNEE].split(':').pop()
+        return verifier.lookup(curHash, function (err, obj) {
+          if (err) return cb(err)
+
+          cb(null, Identity.fromJSON(obj.parsed.data))
+        })
+      }
+
+      var identity = chainedObj.from && chainedObj.from.identity
+      if (identity) return cb(null, identity)
+
+      cb(new Error('unknown sender'))
     }
 
-    var keys = identity.keys({
-      purpose: purpose
-    })
-
-    var unsigned = omit(data, [SIG])
-
-    var rebuild = new Builder().data(unsigned)
-    chainedObj.parsed.attachments.forEach(rebuild.attach, rebuild)
-    rebuild.build(function (err, result) {
+    function verify (err, signee) {
       if (err) return cb(err)
 
-      var buf = result.form
-      var verifications = keys.map(function (k) {
-        return k.verify.bind(k, buf, sig)
+      var isIdentity = data[TYPE] === Identity.TYPE
+      var purpose
+      if (isIdentity && data[PREV_HASH]) {
+        purpose = 'update'
+      } else {
+        purpose = 'sign'
+      }
+
+      var keys = signee.keys({
+        purpose: purpose
       })
 
-      parallel(verifications, function (err, results) {
+      var unsigned = omit(data, [SIG])
+
+      var rebuild = new Builder().data(unsigned)
+      chainedObj.parsed.attachments.forEach(rebuild.attach, rebuild)
+      rebuild.build(function (err, result) {
         if (err) return cb(err)
 
-        if (results.some(function (r) { return r })) {
-          next(verifier, chainedObj, cb)
-        } else {
-          cb(new Error('no key verifies signature'))
-        }
+        var buf = result.form
+        var verifications = keys.map(function (k) {
+          return k.verify.bind(k, buf, sig)
+        })
+
+        parallel(verifications, function (err, results) {
+          if (err) return cb(err)
+
+          if (results.some(function (r) { return r })) {
+            next(verifier, chainedObj, cb)
+          } else {
+            cb(new Error('no key verifies signature'))
+          }
+        })
       })
-    })
+    }
   }
 }
